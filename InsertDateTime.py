213352a94e91f79a4e701b5974cd437cc1bdb898
mattst <mattst@i-dig.info>
2014-10-19 14:30:06 +0100

    Initial commit - bare class file.

---
+++ InsertDateTime.py
@@ -0,0 +1,35 @@
+#
+# Name:             InsertDateTime
+#
+# File:             InsertDateTime.py
+#
+# Requirements:     Plugin for Sublime Text v.2 and v.3
+#
+# Tested:           ST v.3 build 3065 - not tested.
+#                   ST v.2 build 2221 - not tested.
+#
+# Written by:       mattst - https://github.com/mattst
+#
+# Url:              n/a
+#
+# Version:          n/a
+#
+# ST Command:       insert_date_time
+#
+# Description:      See class description below.
+#
+
+import sublime, sublime_plugin
+
+class InsertDateTimeCommand(sublime_plugin.TextCommand):
+    """
+    The InsertDateTimeCommand class is a Sublime Text plugin which...
+    """
+
+    def run(self, edit):
+        """
+        run() is called when the command is run. Duhhh. :)
+        """
+
+
+    # End of def run()
